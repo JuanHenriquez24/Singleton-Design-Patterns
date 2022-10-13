2b063b9ed06a1c6f61ac67a0279d0e7a93f2e76f
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-10-13 09:03:53 -0300

    Did most of the things

---
+++ Design Patterns/Assets/SceneChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] string[] sceneNames;
+    [SerializeField] int sceneIndex;
+
+    public void ChangeSceneSequentially()
+    {
+        if (sceneIndex < sceneNames.Length)
+        {
+            SceneManager.LoadScene(sceneNames[sceneIndex]);
+            sceneIndex++;
+        }
+        else
+        {
+            sceneIndex = 0;
+        }
+    }
+    void ChangeSceneAndIncrementIndex()
+    {
+        SceneManager.LoadScene(sceneNames[sceneIndex]);
+        sceneIndex++;
+    }
+}
